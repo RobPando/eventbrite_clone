17017b6e79502f3eadc7b5cb4cf8c645bf495432
Roberto Pando <roberto_pando@hotmail.com>
2016-11-07 20:11:29 +0100

    implemeneted sessions helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
+      log_in @user
       flash[:success] = "You have successfully logged in!"
       redirect_to @user
     else
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
