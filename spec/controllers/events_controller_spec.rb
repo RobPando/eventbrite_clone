13d7da3caeb28d05a3874650d87aa78ec18ab70a
Roberto Pando <roberto_pando@hotmail.com>
2016-11-10 14:43:51 +0100

    date format tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe EventsController, type: :controller do
+RSpec.describe EventsController do
 
   it "shows all the events" do
     get :index
     expect(response).to have_http_status(:success)
+  end
+
+  it "is invalid if name is blank"
+  it "is invalid if description is blank"
+  it "is invalid if location is blank"
+  it "it invalid if date is blank"
+
+  context "date format" do
   end
 end
